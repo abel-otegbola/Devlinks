a753add5da443ac3670f1bf0449203b761abeecd
abel-otegbola <abel15655@gmail.com>
2024-07-24 07:28:06 +0100

    fix: apos and quot errors in welcomescreen

--- src/components/welcomeScreen/welcomeScreen.tsx
+++ src/components/welcomeScreen/welcomeScreen.tsx
@@ -3,8 +3,8 @@ export default function WelcomeScreen() {
     return (
         <>
         <div className="flex flex-col justify-center items-center min-h-[400px] my-6 md:px-[25%] px-[5%] py-[60px] bg-slate">
-            <h1 className="text-[32px] mb-6 font-bold text-center">Let's get you started</h1>
-            <p className="text-center leading-[24px]">Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them. We're here to help you share your profiles with everyone!</p>
+            <h1 className="text-[32px] mb-6 font-bold text-center">Let&apos;s get you started</h1>
+            <p className="text-center leading-[24px]">Use the &quot;Add new link&quot; button to get started. Once you have more than one link, you can reorder and edit them. We&apos;re here to help you share your profiles with everyone!</p>
         </div>
         </>
     )
