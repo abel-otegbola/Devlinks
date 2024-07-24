79259cb00f32717d8688d1db85d13dc9b1e7de28
abel-otegbola <abel15655@gmail.com>
2024-07-24 01:56:53 +0100

    feat: add welcome screen in links page

--- src/app/links/page.tsx
+++ src/app/links/page.tsx
@@ -1,37 +1,37 @@
+import Button from "@/components/button/button";
 import Header from "@/components/header/header";
+import PreviewSkeleton from "@/components/previewSkeleton/previewSkeleton";
 import Image from "next/image";
 
 export default function Links() {
     return (
-        <>
+        <div className="p-4 bg-slate">
         <Header />
-        <div className="flex flex-wrap ">
-            <div className="md:w-[40%] min-h-screen flex items-center justify-center px-[10%]">
+        <div className="flex flex-wrap gap-6 mt-6">
 
-                <div className="relative w-[308px] h-[632px] bg-cover p-[60px_20px]">
-                    <Image src="/images/phone.svg" alt="preview-phone" width={308} height={632} className="absolute top-0 left-0 z-[-1]" />
-
-                    <div className="flex flex-col items-center gap-6 w-full h-full">
-                        <div className={`bg-[#EEEEEE] rounded-full w-[100px] h-[100px]`} >
-                        </div>
-
-                        <p className="bg-[#EEEEEE] rounded-[32px] h-[16px] w-[160px]"></p>
-                        <p className="bg-[#EEEEEE] rounded-[32px] h-[8px] w-[72px]"></p>
-
-                        <div className="flex flex-col gap-5 py-12">
-                            <p className="bg-[#EEEEEE] rounded-[8px] h-[44px] w-[237px]"></p>
-                            <p className="bg-[#EEEEEE] rounded-[8px] h-[44px] w-[237px]"></p>
-                            <p className="bg-[#EEEEEE] rounded-[8px] h-[44px] w-[237px]"></p>
-                        </div>
-                    </div>
-                </div>
-
+            <div className="md:w-[40%] min-h-screen flex items-center justify-center bg-white rounded-[8px]">
+                <PreviewSkeleton />
             </div>
             
-            <div className="md:w-[60%]">
-                    
+            <div className="flex-1 bg-white rounded-[8px] p-[40px]">
+                <div className="mb-10">
+                    <h1 className="text-[32px] mb-6 font-bold">Customize your links</h1>
+                    <p>Add/edit/remove links below and then share all your profiles with the world!</p>
+                </div>
+                <Button variant="secondary" size="full">+ Add new link</Button>
+
+                <div className="flex flex-col justify-center items-center min-h-[400px] my-6 md:px-[25%] px-[5%] py-[60px] bg-slate">
+                    <h1 className="text-[32px] mb-6 font-bold text-center">Let's get you started</h1>
+                    <p className="text-center leading-[24px]">Use the "Add new link" button to get started. Once you have more than one link, you can reorder and edit them. We're here to help you share your profiles with everyone!</p>
+
+                </div>
+                
+                <div className="flex justify-end">
+                    <Button disabled={true} >Save</Button>
+                </div>
             </div>
+
         </div>
-        </>
+        </div>
     )
 }
